a6ddff3ca9c04c75e42cc8b55368812886765088
bugPirates <ican.notfound@gmail.com>
2022-04-05 10:58:31 +0800

    Push struktur02

--- pages/struktur.js
+++ pages/struktur.js
@@ -1,4 +1,4 @@
-import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import { Container, Heading } from '@chakra-ui/react'
 import Image from 'next/dist/client/image'
 import Layout from '../components/layouts/article'
 import Footer from '../components/footer'
